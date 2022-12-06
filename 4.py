1a27cc256e83b4ca4b7724b8777f83b0e2a42a36
bookbinder <carlsenr@gmail.com>
2022-12-06 14:27:12 -0500

    update 4

--- 4.py
+++ 4.py
@@ -3,12 +3,13 @@ from functools import partial
 from typing import Tuple
 
 def overlap(line: Tuple[int], part2=False) -> bool:
-    """Accepts a tuple of 4 ints. Determines if ranges s1-e1 and
-    s2-e2 overlap. Total overlap for part1. For part2, can be partial."""
-    s1, e1, s2, e2 = line
+    """Creates 2 ranges from the tuple of 4 ints and checks for overlap: total
+    overlap in part 1, and at least partial overlap in part 2"""
+    a = range(line[0], line[1] + 1)
+    b = range(line[2], line[3] + 1)
     if part2:
-        return (s1 <= s2 <= e1) or (s2 <= s1 <= e2)
-    return (s1 <= s2 and e1 >= e2) or (s1 >= s2 and e1 <= e2)
+        return (a[0] in b) or (b[0] in a)
+    return all([z in b for z in a]) or all([z in a for z in b])
 
 input = tuple(map(rc.ints, rc.aoc_in(__file__)[1].splitlines()))
 print("Part 1:", sum(map(overlap, input)))
--- rctools.py
+++ rctools.py
@@ -2,15 +2,11 @@
 
 from typing import Tuple
 
-
 # some useful variables
-
 Lc = "abcdefghijklmnopqrstuvwxyz"
 Uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-
 # some useful functions
-
 def ints(s: str) -> Tuple[int]:
     """Return all numbers in s as a tuple of ints. A dash before a number
     indicates negative unless the dash is preceded by a number."""
@@ -19,10 +15,10 @@ def ints(s: str) -> Tuple[int]:
 
 def aoc_in(day: str) -> Tuple[str]:
     """Accepts file name `day` and converts it to just the digits in
-    the stem of the filename. (4b.py == 4.py == 4)
-    Returns tuple of two file strings: the first for example data
-    (or empty string) and the second for the real data (or empty) for the
-    corresponding day in data subdirectory."""
+    the stem of the filename. (4b.py == 4.py == 4) Returns tuple of two
+    file strings: the first for example data (or empty string) and the
+    second for the real data (or empty) for the corresponding day in
+    data subdirectory."""
     from pathlib import Path
     day = ''.join(z for z in Path(day).stem if z.isdigit())
     p1 = Path(f'data/{day}ex.txt')
