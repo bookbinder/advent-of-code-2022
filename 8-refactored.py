4b5867102843863ffbd8591c9ab9725ab30ec202
bookbinder <carlsenr@gmail.com>
2022-12-08 17:06:09 -0500

    update 8

--- 8-refactored.py
+++ 8-refactored.py
@@ -1,35 +1,35 @@
-"""Less repetitive, probably less memory, but slower"""
-
 import rctools as rc
+from typing import Tuple
 from math import prod
 
-def up(r, c):    return (grid[nr][c] for nr in range(r - 1, -1, -1))
-def down(r, c):  return (grid[nr][c] for nr in range(r + 1, R))
-def right(r, c): return (grid[r][nc] for nc in range(c + 1, C))
-def left(r, c):  return (grid[r][nc] for nc in range(c - 1, -1, -1))
+def up(r, c):    return (GRID[nr][c] for nr in range(r - 1, -1, -1))
+def down(r, c):  return (GRID[nr][c] for nr in range(r + 1, R))
+def right(r, c): yield from GRID[r][c + 1:]
+def left(r, c):  yield from reversed(GRID[r][:c])
 
-def is_visible(r: int, c: int):
+def is_visible(pt: Tuple[int]):
     "Whether the treetop at r, c can be seen from any direction outside forest"
-    for f in dirs:
-        if all(z < grid[r][c] for z in f(r, c)):
+    r, c = pt
+    for f in DIRS:
+        if all(z < GRID[r][c] for z in f(r, c)):
             return True
     return False
 
-def viewing_dist(r: int, c: int, dir) -> int:
+def viewing_dist(pt: Tuple[int], dir) -> int:
     "Viewing distance along the dir"
-    return next((i for i, v in enumerate(dir(r, c), 1) if v >= grid[r][c]),
-                 len(list(dir(r, c))))
+    r, c = pt
+    return next((i for i, v in enumerate(dir(r, c), 1) if v >= GRID[r][c]),
+                len(list(dir(r, c))))
 
-def scenic_score(r: int, c: int) -> int:
+def scenic_score(pt: Tuple[int]) -> int:
     "Mulitply viewing distances along 4 axes"
-    return prod(viewing_dist(r, c, dir) for dir in dirs)
+    return prod(viewing_dist(pt, dir) for dir in DIRS)
 
 input = rc.aoc_in(__file__)[1].splitlines()
-grid = tuple(tuple(map(int, r)) for r in input)
-R, C = len(grid), len(grid[0])
-dirs = (up, down, right, left)
+GRID = tuple(tuple(map(int, r)) for r in input)
+R, C = len(GRID), len(GRID[0])
+COORDS = tuple((r, c) for r in range(R) for c in range(C))
+DIRS = (up, down, right, left)
 
-print("Part 1:", sum(is_visible(r, c)
-                     for r in range(R) for c in range(C)))
-print("Part 2:", max(scenic_score(r, c)
-                     for r in range(R) for c in range(C)))
+print("Part 1:", sum(map(is_visible, COORDS)))
+print("Part 2:", max(map(scenic_score, COORDS)))
