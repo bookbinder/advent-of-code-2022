407f0dc64f44ec8e151ac86f1bc1bb13b13cb80e
bookbinder <carlsenr@gmail.com>
2022-12-15 21:02:57 -0500

    day 14

--- 14.py
+++ 14.py
@@ -1,13 +1,8 @@
 import rctools as rc
 import itertools as it
+from copy import deepcopy
 
-def parse(blocked, line):
-    for a, b in it.pairwise(line):
-        for r in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
-            for c in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
-                blocked.add((r, c))
-
-def falling(blocked, r, c, part2=False):
+def falling(blocked, part2=False, r=0, c=500):
     if r > abyss or (0, 500) in blocked:
         return True
     while (r + 1, c) not in blocked and r < abyss:
@@ -16,24 +11,28 @@ def falling(blocked, r, c, part2=False):
         blocked.add((r, c))
         return False
     if (r + 1, c - 1) not in blocked:
-        return falling(blocked, r + 1, c - 1, part2)
+        return falling(blocked, part2, r + 1, c - 1)
     elif (r + 1, c + 1) not in blocked:
-        return falling(blocked, r + 1, c + 1, part2)
+        return falling(blocked, part2, r + 1, c + 1)
     else:
         blocked.add((r, c))
         return False
 
-def calc(part2=False):
-    blocked = set()
-    for line in input:
-        parse(blocked, line)
-    return next(i for i in it.count() if falling(blocked, 0, 500, part2))
-
+# split the input and get max r value + 1 for abyss
 input = rc.aoc_in(__file__)[1].splitlines()
 input = [z.split(' -> ') for z in input]
 input = [[z.split(',') for z in subl] for subl in input]
 input = [[[int(z) for z in subla][::-1] for subla in sublb] for sublb in input]
 abyss = max(int(z[0]) for subl in input for z in subl) + 1
 
-print("Part 1:", calc())
-print("Part 2:", calc(part2=True))
+# create set of all blocked coordinates and make a copy for both parts
+blocked = set()
+for line in input:
+    for a, b in it.pairwise(line):
+        for r in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
+            for c in range(min(a[1], b[1]), max(a[1], b[1]) + 1):
+                blocked.add((r, c))
+blockedB = deepcopy(blocked)
+
+print("Part 1:", next(i for i in it.count() if falling(blocked)))
+print("Part 2:", next(i for i in it.count() if falling(blockedB, part2=True)))
