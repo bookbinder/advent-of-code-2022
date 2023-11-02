7f488b44f8a44a147e0aee4a6b07bd8abf4167ee
bookbinder <carlsenr@gmail.com>
2023-11-02 15:48:43 -0400

    8

--- 8.lisp
+++ 8.lisp
@@ -1,34 +1,24 @@
 (load "util.lisp")
 
-(defun trees-right (grid r c RR CC)
-  "All the trees to the right of R C in GRID."
-  (loop for i from (1+ c) below CC collect (aref grid r i)))
-
-(defun trees-left (grid r c RR CC)
-  "All the trees to the left of R C in GRID."
-  (loop for i downfrom (1- c) to 0 collect (aref grid r i)))
-
-(defun trees-below (grid r c RR CC)
-  "All the trees below R C in GRID."
-  (loop for i from (1+ r) below RR collect (aref grid i c)))
-
-(defun trees-above (grid r c RR CC)
-  "All the trees above R C in GRID."
-  (loop for i downfrom (1- r) to 0 collect (aref grid i c)))
+(defun trees-surrounding (grid r c RR CC)
+  "Neighboring trees below, above, left, and right of R C."
+  (list
+   (loop for i downfrom (1- r) to 0 collect (aref grid i c))
+   (loop for i from (1+ r) below RR collect (aref grid i c))
+   (loop for i downfrom (1- c) to 0 collect (aref grid r i))
+   (loop for i from (1+ c) below CC collect (aref grid r i))))
 
 (defun viewing-distance (cur L)
-  "Viewing distance given current tree CUR and neighboring trees L."
+  "Viewing distance given current tree CUR and neighboring trees in one direction."
   (let ((cand (find-if #'(lambda (x) (>= x cur)) L)))
     (if cand
 	(1+ (position cand L))
 	(length L))))
 
-(defun scenic-score (grid r c RR CC)
+(defun scenic-score (cur L)
   "Scenic score as a product of viewing distances along 4 axes."
-  (reduce #'*
-	  (mapcar #'(lambda (x)
-		      (viewing-distance (aref grid r c) (funcall x grid r c RR CC)))
-		  '(trees-above trees-below trees-left trees-right))))
+  (reduce #'* (mapcar #'(lambda (x) (viewing-distance cur x))
+		      L)))
 
 
 (let* ((input (parse "data/8.txt" #'lines))
@@ -47,9 +37,11 @@
   ;; Find answers for parts 1 and 2
   (dotimes (r RR (list part1 part2))
     (dotimes (c CC)
-      (when (some #'(lambda (x)
-		      (> (aref grid r c)
-			 (apply #'max (or (funcall x grid r c RR CC) (list -1)))))
-		  '(trees-above trees-below trees-left trees-right))
-	(incf part1))
-      (setf part2 (max part2 (scenic-score grid r c RR CC))))))
+      (let ((neigh (trees-surrounding grid r c RR CC)))
+	(when (some #'(lambda (x)
+			(> (aref grid r c)
+			   (apply #'max (or x (list -1)))))
+		    neigh)
+	  (incf part1))
+	(setf part2 (max part2
+			 (scenic-score (aref grid r c) neigh)))))))
