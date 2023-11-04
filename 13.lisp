3fed17170aa668ef617b4a3df644fcd66b722420
bookbinder <carlsenr@gmail.com>
2023-11-04 19:58:05 -0400

    13

--- 13.lisp
+++ 13.lisp
@@ -1,15 +1,16 @@
 (load "util.lisp")
 
-(defun my-eval (s)
-  "Parse string S into a quoted list data structure."
-  (eval (read-from-string (format nil "~A~A" "'" s))))
-
-(defun repl (s)
-  "Make string S look like a Lisp list data structure."
-  (substitute #\( #\[
-	      (substitute #\) #\]
-			  (substitute #\Space #\,
-				      s))))
+(defun my-eval (L)
+  "Convert a list of two strings into two proper sublists using eval."
+  (mapcar #'(lambda (x)
+	      (eval
+	       (read-from-string
+		(format nil "~A~A"
+			"'"
+			(substitute #\( #\[
+				    (substitute #\) #\]
+						(substitute #\Space #\, x)))))))
+	  L))
 
 (defun compare (a b)
   "Compare A and B for sorting."
@@ -27,16 +28,15 @@
 			  (signum val)))))))
     (< (rec a b) 0)))
 
-(let* ((input (mapcar #'lines (pars (repl (parse "data/13.txt")))))
-       (input (mapcar #'(lambda (x) (mapcar #'my-eval x)) input))
+(let* ((input (parse "data/13.txt" #'pars #'lines #'my-eval))
        (part1 0))
   (do ((i 1 (1+ i))
        (arr input (cdr arr)))
       ((null arr))
     (when (compare (first (car arr)) (second (car arr)))
       (incf part1 i)))
-  (let ((p2-input (sort (apply #'append '(((2))) '(((6))) input) #'compare)))
+  (let ((part2 (sort (apply #'append '(((2))) '(((6))) input) #'compare)))
     ;; Return answers for parts 1 and 2:
     (list part1
-	  (* (1+ (position '((6)) p2-input :test 'equalp))
-	     (1+ (position '((2)) p2-input :test 'equalp))))))
+	  (* (1+ (position '((6)) part2 :test 'equal))
+	     (1+ (position '((2)) part2 :test 'equal))))))
