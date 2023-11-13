3830927db6ca1a4fc3289ddc86f77aa86d2f1bfe
bookbinder <carlsenr@gmail.com>
2023-11-13 15:41:50 -0500

    17

--- 17.lisp
+++ 17.lisp
@@ -1,88 +1,113 @@
 (load "util.lisp")
 
-(let* ((input  (map 'list #'(lambda (x)
-			      (case x (#\> 1) (#\< -1)))
-		    (parse "data/17.txt")))
-       (wind   (make-array (length input) :initial-contents input))
-       (wn     (length wind))
-       (wi     (- wn 1))
-       (shapes '(((0 0) (0 1) (0 2) (0 3))
-		 ((0 1) (1 0) (1 1) (1 2) (2 1))
-		 ((0 2) (1 2) (2 0) (2 1) (2 2))
-		 ((0 0) (1 0) (2 0) (3 0))
-		 ((0 0) (0 1) (1 0) (1 1))))
-       (sn     (length shapes))
-       (si     (- sn 1)))
-  (defun next-wind ()
-    (incf wi)
-    (aref wind (mod wi wn)))
-  (defun next-shape ()
-    (incf si)
-    (nth (mod si sn) shapes))
-  (defun cur-shape () (mod si sn))
-  (defun cur-wind () (mod wi wn)))
-
-(defparameter *grid* (make-array '(4000 7) :initial-element 0))
-(defparameter *mx* (first (array-dimensions *grid*)))
-(defparameter *rr* (first (array-dimensions *grid*)))
-(defparameter *cache* (make-hash-table :test 'equal))
-(defparameter *last5* (make-list 5 :initial-element nil))
-(defparameter *cycle* nil)
-
-(defun shift (shape dir)
-  "Shift piece in direction DIR if possible. If possible, return new coords
-and t, otherwise old coords and nil."
+(defun shift (grid shape dir)
+  "Shift piece one unit in direction DIR if possible. Return new coords or nil."
   (let* ((dr (if (zerop dir) 1 0))
 	 (dc dir)
 	 (new-shape (mapcar #'(lambda (x)
 				(list (+ dr (first x)) (+ dc (second x))))
 			    shape)))
-    (if (or (some #'(lambda (x)
+    (unless (or (some #'(lambda (x)
 		      (or (not (<= 0 (second x) 6))
-			  (>= (first x) *rr*)))
+			  (>= (first x) (first (array-dimensions grid)))))
 		  new-shape)
 	    (some #'(lambda (x)
-		      (= 1 (aref *grid* (first x) (second x))))
+		      (= 1 (aref grid (first x) (second x))))
 		  new-shape))
-	(values shape nil)
-	(values new-shape t))))
+      new-shape)))
 
-(defun pos (shape r &optional (c 2))
-  "Get starting coordinates for shape."
-  (mapcar #'(lambda (x) (mapcar #'+ x (list r c))) shape))
-
-(defun drop (shape i)
-  "Drop a shape and let it fall until it rests."
-  (let* ((r     (- *mx* 4))
-	 (shape (pos shape (- r (- (apply #'max (mapcar #'car shape))
-				   (apply #'min (mapcar #'car shape)))))))
+(defun drop (grid i j mx cycle &optional (sig nil sig-p))
+  "Drop a shape until it rests. I is rock count, J is wind count, MX is
+max height so far, and CYCLE is whether a cycle has been found. SIG
+checks for a signature for repetition. Derive shape from I and position
+of shape from MX, and return new grid, J, MX, and cycle."
+  (let* ((is    (mod i *ns*))
+	 (r     (- mx (+ 4 (- (nth is *shape-heights*) 1))))
+	 (shape (mapcar #'(lambda (x)
+			    (list (+ r (first x)) (+ 2 (second x))))
+			(nth is *shapes*))))
     (while t
-      (setf shape (shift shape (next-wind)))
-      (multiple-value-bind (newshape res) (shift shape 0)
-	(if res
+      (when-bind (val (shift grid shape (aref *wind* (mod j *nw*))))
+	(setf shape val))
+      (incf j)
+      (let ((newshape (shift grid shape 0))) 
+	(if newshape
 	    (setf shape newshape)
 	    (progn
 	      (dolist (z shape)
-		(setf (aref *grid* (first z) (second z)) 1))
-	      (setf *last5* (butlast *last5*))
-	      (push (mapcar #'second shape) *last5*)
-	      (when-bind (val (gethash (apply #'append *last5*) *cache*))
-		(when (equal (list (cur-shape) (cur-wind))
-			     (subseq val 0 2))
-		  (setf *cycle* (list (list (third val) (fourth val))
-				      (list *mx* i)))
-		  (return-from drop)))
-	      (setf (gethash (apply #'append *last5*) *cache*)
-		    (list (cur-shape) (cur-wind) *mx* i))
-	      (setf *mx* (min *mx* (apply #'min (mapcar #'car shape))))
-	      (return-from drop)))))))
+		(setf (aref grid (first z) (second z)) 1))
+	      (let ((new-mx (min mx (apply #'min (mapcar #'car shape)))))
+		(when (and sig-p (null cycle))
+		  (when-bind (val (funcall sig grid mx i j))
+		    (setf cycle val)))
+		(return-from drop (list grid j new-mx cycle)))))))))
 
-(dotimes (i 3022 (- *rr* *mx*))
-  (drop (next-shape) i)
-  (when *cycle* (return)))
+(defun signature (&optional (n 20))
+  "Return a function that converts last N rows of grid into a number.
+Store results in MEMO with
+key= (current-shape-index current-wind-index last-n-rows-number), and
+value= (current-rock-num mx). When the inner function finds a
+repetition, return values at the 2 indices, otherwise nil."
+  (let ((memo (make-hash-table :test 'equal)))
+    #'(lambda (grid mx i j)
+	(let* ((rows (parse-integer
+		      (format nil "~{~A~}"
+			      (loop for x from (max (- mx n) 0) below mx
+				    append (loop for y below 7
+						 collect (aref grid x y))))
+		      :radix 2))
+	       (val (gethash (list (mod i *ns*) (mod j *nw*) rows) memo)))
+	  (if val
+	      (list val (list i mx))
+	      (progn
+		(setf (gethash (list (mod i *ns*) (mod j *nw*) rows) memo)
+		      (list i mx))
+		nil))))))
 
-;; (let* ((start (first (first cycle)))
-;;        (end (first (second cycle)))
-;;        (height (- end start))))
+;;;; Global constants
+(defparameter *input*
+  (map 'list #'(lambda (x) (case x (#\> 1) (#\< -1)))
+       (parse "data/17.txt")))
+(defparameter *wind* (make-array (length *input*) :initial-contents *input*))
+(defparameter *nw* (length *wind*))
+(defparameter *shapes* '(((0 0) (0 1) (0 2) (0 3))
+			 ((0 1) (1 0) (1 1) (1 2) (2 1))
+			 ((0 2) (1 2) (2 0) (2 1) (2 2))
+			 ((0 0) (1 0) (2 0) (3 0))
+			 ((0 0) (0 1) (1 0) (1 1))))
+(defparameter *ns* (length *shapes*))
+(defparameter *shape-heights*
+  (mapcar #'(lambda (x) (1+ (apply #'max (mapcar #'first x)))) *shapes*))
+(defparameter *max-rocks* 1000000000000)
 
-(print *cycle*)
+
+(let* ((n     3000)  ; max number of rocks to drop
+       (grid  (make-array (list (+ 10 (* n 4)) 7) :initial-element 0))
+       (mx    (first (array-dimensions grid)))
+       (j     0)     ; wind count
+       (cycle nil)   ; have we encountered a repition of the signature yet?
+       (part1 nil)
+       (sig (signature))
+       rock-heights)
+  (dotimes (i n (list part1 "cycle not found"))
+    (let ((val (drop grid i j mx cycle sig)))
+      (setf grid  (first val)
+	    j     (second val)
+	    mx    (third val)
+	    cycle (fourth val))
+      (push mx rock-heights))
+    (when (= i 2021)
+      (setf part1 (- (first (array-dimensions grid)) mx)))
+    (when (and part1 cycle)
+      (destructuring-bind ((rockstart mxstart) (rockend mxend)) cycle
+	  (let* ((htstart (- (first (array-dimensions grid)) mxstart))
+		 (htend (- (first (array-dimensions grid)) mxend))
+		 (cycle-height (- htend htstart))
+		 (cycle-length (- rockend rockstart)))
+	    (multiple-value-bind (num-cycles remainder)
+		(floor (- *max-rocks* rockstart) cycle-length)
+	      (return (list part1
+			    (+ (* num-cycles cycle-height)
+			       (- (first (array-dimensions grid))
+				  (nth (1- (+ remainder rockstart))
+				       (reverse rock-heights))))))))))))
